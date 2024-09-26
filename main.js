e58cf9795c0225806db4f944e815f910ca94ef2c
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-09-26 11:51:05 -0300

    main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ alert('Luiz 24 e Willian 39')
 function soma (num1,num2){
     return num1*num2;
 }
+prompt('lado do quadrado')
 function lado (let lado = prompt ('lado do quadrado');
 }
 let area = lado**2
