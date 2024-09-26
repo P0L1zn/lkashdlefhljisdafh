3841adb2ea19023cb4f9fb750ce10e7434a30ef1
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-09-26 11:37:35 -0300

    main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ alert('Luiz 24 e Willian 39')
 function soma (num1,num2){
     return num1*num2
 }
-let num1=5
-let num2=4
-console.log (soma (num1,num2))
+function area (l1,l2){
+    return l1*l2;
+}
+console.log (soma (5,8))
