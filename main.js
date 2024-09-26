0d18ca92ab4fefb2f4ee22be074e0efd282963cf
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-09-26 12:09:28 -0300

    main.js

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ let lado = parseInt (prompt('lado do quadrado'));
  let area = lado**2
 console.log (soma (5,8));
 console.log (area);
+let total = parseInt (prompt ('media de 3 números')
+let media = total/3
+console.log (media);
