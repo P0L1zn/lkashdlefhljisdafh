4d9d6ed7306198fa66257822326934e981bb3e0a
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-10-03 11:34:36 -0300

    main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 alert('Luiz 24 e Willian 39')
-function soma1 (num1,num2){
-    return num1*num2;
-}
-let lado = parseInt (prompt('lado do quadrado'));
- let area = lado**2
-console.log (soma (5,8));
-console.log (area);
-let valor1 = parseInt (prompt('primeiro número'))
-let valor2 = parseInt (prompt('segundo número'))
-let valor3 = parseInt (prompt('terceiro número'))
-function soma(valor1, valor2, valor3) {
-    return valor1 + valor2 + valor3;
-}
-let total = soma(valor1, valor2, valor3);
-let media = total/3
-console.log ('a média dos três números é:',media);
+// function soma1 (num1,num2){
+//     return num1*num2;
+// }
+// let lado = parseInt (prompt('lado do quadrado'));
+//  let area = lado**2
+// console.log (soma (5,8));
+// console.log (area);
+// let valor1 = parseInt (prompt('primeiro número'))
+// let valor2 = parseInt (prompt('segundo número'))
+// let valor3 = parseInt (prompt('terceiro número'))
+// function soma(valor1, valor2, valor3) {
+//     return valor1 + valor2 + valor3;
+// }
+// let total = soma(valor1, valor2, valor3);
+// let media = total/3
+// console.log ('a média dos três números é:',media);
 let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'SC', 'SE', 'SP', 'TO'];
