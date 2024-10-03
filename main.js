2f885fb3ec795abe276e9c5bf3442744fd92927d
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-10-03 11:59:17 -0300

    main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ alert('Luiz 24 e Willian 39')
 // console.log ('a média dos três números é:',media);
 let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 let i = 0;
-while (i<10){
+while (i<27){
 console.log (i);
 i=i+1;
 }
