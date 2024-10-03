19615a5080b5c2a5157db051ab38b8e75c470562
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-10-03 12:09:03 -0300

    main.js

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ alert('Luiz 24 e Willian 39')
 let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 let i = 0;
 while (i<27){
+console.log (i); 
 console.log (estados[i]);
 i=i+1;
 }
