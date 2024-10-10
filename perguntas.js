e7c3b1bd7900220bab9405b6eb7bebad07791729
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-10-10 11:48:39 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao('Matemática',
+    'Qual o teorema de Pitágoras?',
+    'a²+b²=c²')
+    criaCartao('Matemática',
+    'Qual a fórmula da área do triângulo?',
+    'b.h/2')
+    criaCartao('História',
+    'Onde ocorreu a Primeira Revolução Industrial?',
+    'Inglaterra')
+    criaCartao('História',
+    'Qual o país reconhecido como mais antigo do mundo?',
+    'Egito')
+    criaCartao('Educação Financeira',
+    'O que é Recisão de Contrato?',
+    'Término de um vínculo empregatício')
+    criaCartao('Educação Financeira',
+    'Qual o intervalo mínimo de horas de uma interjornada?'
+    '11 horas'),
+    criaCartao('Física',
+    'Qual a fórmula da Velocidade?',
+    'V=Δs/Δt')
+    criaCartao('Matemática',
+    'Qual a fórmula da Circunferência?',
+    'C=2πr')
+    criaCartao('Biotecnologia ',
+    'O que é Alporquia?',
+    'Método de propagação vegetativa')
+    criaCartao('História',
+    'Em qual ano se iniciou a Revolução Francesa?',
+    '1789')
+    criaCartao('Química','O que é a cadeia carbônica principal?','A sequência com maiores números de carbonos seguindo a regra de, grupo funcional, insaturação e radicais')
+    criaCartao('Química','Qual o nome de um radical com 3 carbonos?','propil')
+    criaCartao('química','Quantos carbonos possuem os radicais nomeados metil?','1')
+    criaCartao('Química','Quantas ligações o carbono faz?','4')
+    criaCartao('Química','O que é uma ligação covalente?','uma ligação na qual átomos "emprestam" seus elétrons')
+    criaCartao('Química','O que é uma reação exotérmica?','Uma reação que libera calor')
+    criaCartao('Química','O que é uma reação','É quando dois ou mais compostos reagem formando um produto')
+    criaCartao('Química','O que é camada de valência','É a camada no qual os elétrons estão posicionados')
+    criaCartao('Química','Quantos elementos existem na tabela periódica','118')
+    criaCartao('Química','O que é uma insaturação','É quando ocorre uma ligação que dupla ou tripla')
