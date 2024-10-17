b4cb5de1fbba8e9ebacb1407b1a6ff958156e807
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-10-17 11:49:42 -0300

    main.js

--- main.js
+++ main.js
@@ -1,45 +1,17 @@
-alert('Luiz 24 e Willian 39')
-// function soma1 (num1,num2){
-//     return num1*num2;
-// }
-// let lado = parseInt (prompt('lado do quadrado'));
-//  let area = lado**2
-// console.log (soma (5,8));
-// console.log (area);
-// let valor1 = parseInt (prompt('primeiro número'))
-// let valor2 = parseInt (prompt('segundo número'))
-// let valor3 = parseInt (prompt('terceiro número'))
-// function soma(valor1, valor2, valor3) {
-//     return valor1 + valor2 + valor3;
-// }
-// let total = soma(valor1, valor2, valor3);
-// let media = total/3
-// console.log ('a média dos três números é:',media);
-// let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-// let i = 0;
-// while (i<estados.length){
-// console.log (i); 
-// console.log (estados[i]);
-// i=i+1;
-// }
-// for (let j = 0; j< estados.length; j++) {
-//     console.log (j)
-//     console.log (estados[j]);
-// }
-// let idade = 16
-// if (idade) { 
-//     idade<18
-// console.log ('não pode beber')
-// }
-// else {
-//     console.log ('pode beber')
-// }
 function criaCartao (categoria, pergunta, resposta){
     let container = document.getElementById ('container');
     let cartao = document.createElement('article')
 cartao.className =  'cartao';
-cartao.innerHTML = '
+cartao.innerHTML = `
 <div class= "conteudo-cartao">
-
-'
+<h3>${categoria}</h3>
+<div class="pergunta-cartao">
+<p>${pergunta}</p>
+</div>
+<div class="resposta-cartao">
+<p>${resposta}</p>
+</div>
+</div>
+`
+container.appendChild(cartao);
 }
