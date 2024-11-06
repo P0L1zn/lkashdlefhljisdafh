7de81f8c755e63a069ac7f93209acd25de024901
P0L1zn <poli.luiz@escola.pr.gov.br>
2024-11-06 08:40:37 -0300

    pagina2.js

--- pagina2.js
+++ pagina2.js
@@ -17,7 +17,7 @@ cartao.innerHTML = `
 container.appendChild(cartao);
 }
 function mostraResposta (card) {
-    const resposta = card.querySelector ('.respostaCartao');
+    const resposta = card.querySelector ('.resposta-cartao');
     if (resposta.style.display==='none') {
         resposta.style.display='block';
     }
